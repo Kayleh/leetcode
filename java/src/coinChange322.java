2033d8e534a69fef13b2889d8d4cbebbd9767e83
Kayleh <yao924934908@yeah.net>
2021-03-17 02:23:17 +0800

    commit

--- java/src/coinChange322.java
+++ java/src/coinChange322.java
@@ -1,39 +1,34 @@
+import java.util.Arrays;
+
 /**
  * @Author: Kayleh
  * @Date: 2021/3/17 1:13
  */
+//dp数组
 public class coinChange322
 {
-    public int coinChange(int[] coins, int amount)
+
+
+    public int coinChange(int amount, int[] coins)
     {
 
-        int[] memo = new int[amount + 1];
-        return dp(memo, amount, coins);
-    }
-
-    public static int dp(int[] memo, int amount, int[] coins)
-    {
-
-        if (amount == 0) return 0;
-        if (amount < 0) return -1;
-        if (memo[amount] != 0) return memo[amount];
-        int res = Integer.MAX_VALUE;
-        for (int coin : coins)
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; i++)
         {
-            int subProblem = dp(memo, amount - coin, coins);
-            if (subProblem == -1) continue;
-            res = Math.min(res, subProblem + 1);
+            for (int coin : coins)
+            {
+                if (i - coin < 0) continue;
+                dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+            }
         }
-        if (res != Integer.MAX_VALUE)
-        {
-            memo[amount] = res;
-        } else
-        {
-            memo[amount] = -1;
-        }
-        return memo[amount];
+
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 }
+
+//记忆化搜索
 //    public int coinChange(int[] coins, int amount)
 //    {
 //
