c12280441d9fdb90d8405e535ad2b365e0d78052
Kayleh <yao924934908@yeah.net>
2021-07-16 00:39:53 +0800

    commit

--- java/src/tree/inorderTraversal94.java
+++ java/src/tree/inorderTraversal94.java
@@ -7,15 +7,19 @@ import java.util.List;
  * @Author: Kayleh
  * @Date: 2021/5/12 16:53
  */
-public class inorderTraversal94
-{
-    public List<Integer> inorderTraversal(TreeNode root)
-    {
-        List<Integer> list = new ArrayList<>();
-        TreeNode head = root;
-        if (head == null) return list;
-        list.add(head.val);
-        if (head.left != null) inorderTraversal(head.left);
-        return list;
+public class inorderTraversal94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
     }
 }
